baba0aea74648995d4ff73cbe53d0d0ea3693b79
Justin Alemany <justin.alemany@gmail.com>
2018-05-29 12:57:27 +0200

    sauvegarde 13h
    pour ne pas perdre le code entre 13h et 14h

--- AspMvcKit/AspMvcKit/Controllers/HomeController.cs
+++ AspMvcKit/AspMvcKit/Controllers/HomeController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using System.IO;
 
 namespace AspMvcKit.Controllers
 {
+
     public class HomeController : Controller
     {
+
         // GET: Home
         public ActionResult Index()
         {
+
             return View();
+
         }
+
     }
+
 }
